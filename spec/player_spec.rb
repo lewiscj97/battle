3f2dae91f5942587c5801cc61dfd6a3fb177099a
Tom Baxter <thomas99baxter@gmail.com>
2021-10-05 16:24:22 +0100

    add attack functionality

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,29 @@ require 'player'
 
 describe Player do
 
-  it 'should return the player name' do
-    expect(Player.new('foo').name).to eq 'foo' 
+  describe '#initialize' do
+    it 'should return the player name' do
+      expect(described_class.new('foo').name).to eq 'foo' 
+    end
+  
+    it 'should return the player hp' do
+      expect(described_class.new('foo').hp).to eq 100 
+    end
+  end
+  
+  describe '#attack' do
+    it 'should decrement player hp by 10 when attacked' do
+      test_player = Player.new('foo')
+  
+      expect{test_player.attack}.to change { test_player.hp }.from(100).to(90)
+    end
+
+    it 'should decrement player hp by 10 when attacked' do
+      test_player = Player.new('foo')
+  
+      expect{
+        5.times do test_player.attack end
+      }.to change { test_player.hp }.from(100).to(50)
+    end
   end
 end
